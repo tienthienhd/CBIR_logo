34f7021a3b3577f1159fadfd9e0f1a42f6005917
huyphuong99 <phuong.nguyenhuy.hust@gmail.com>
2021-11-07 15:17:33 +0700

    update api check logo and add logo to json

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,19 @@
 import cv2.cv2
+import numpy as np
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 from sift_feature.sift_query import Query_Image
+
+from flask_restful import Resource, reqparse
+import werkzeug
+import requests
+import ssl
+from urllib3 import poolmanager
+import re
+import io
+import  base64
+from PIL import Image
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 url_query = None
@@ -11,27 +22,114 @@ url_img_results = []
 app = Flask(__name__)
 
 from sift_feature.query import query_img, match_and_box
-import json
 
 
 # with open("sift_feature/dict_logo.json") as json_file:
 #     data = json.load(json_file)
+# @timeit
+class TLSAdapter(requests.adapters.HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        """Create and initialize the urllib3 PoolManager."""
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        self.poolmanager = poolmanager.PoolManager(
+                num_pools=connections,
+                maxsize=maxsize,
+                block=block,
+                ssl_version=ssl.PROTOCOL_TLS,
+                ssl_context=ctx)
 
+
+def url_to_image(url):
+    """Download image from url"""
+    session = requests.session()
+    session.mount('https://', TLSAdapter())
+    res = session.get(url, timeout=5)
+    session.close()
+    image = np.asarray(bytearray(res.content), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+def is_support_type(filename):
+    """Check type support"""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def stream_to_image(stream):
+    """Parser image in bytes"""
+    npimg = np.fromstring(stream.read(), np.int8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    return img
+
+def string_to_image(img_string):
+    """Parser image from base64"""
+    img_string = re.sub('^data:image/[a-z]+;base64,', '', img_string)
+    imgdata = base64.b64decode(img_string)
+
+    image = Image.open(io.BytesIO(imgdata))
+    img = np.array(image)
+
+    if img.ndim == 3 and img.shape[2] > 3:
+        img = img[:, :, :3]
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+
+class ImageException(Exception):
+    pass
+
+def parse_request(images):
+    imgs = []
+    filenames = []
+    for image in images:
+        img = None
+        filename = None
+        try:
+            if isinstance(image, str):
+                if len(image) < 2:
+                    raise ImageException("String image is wrong format")
+                if image.startswith("http"):
+                    img = url_to_image(image)
+                    filename = "url"
+                else:
+                    img = string_to_image(image)
+                    filename = "base64"
+            elif isinstance(image, werkzeug.datastructures.FileStorage):
+                if is_support_type(image.filename):
+                    img = stream_to_image(image)
+                    filename = image.filename
+                else:
+                    raise ImageException("Image is wrong format")
+
+        except Exception as e:
+            raise e
+        imgs.append(img)
+        filenames.append(filename)
+    # TODO: Optimize performance without log_image
+    # log_image(imgs)
+    return imgs, filenames
+
+
+def parse_args(*args):
+    parser = reqparse.RequestParser()
+    parser.add_argument('image', required=True, location=['form', 'args', 'files', 'json'],
+                        action='append')
+    if len(args) > 0:
+        for para in args:
+            parser.add_argument(para, location=['form', 'args', 'files', 'json'])
+    args_ = parser.parse_args(strict=True)
+    if len(args_['image']) > 0 and "FileStorage" in args_['image'][0]:
+        parser.replace_argument('image', type=werkzeug.datastructures.FileStorage, required=True, location='files',
+                                action='append')
+    args_ = parser.parse_args(strict=True)
+
+    images = args_['image']
+    imgs, filenames = parse_request(images)
+    return imgs, filenames
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-# @app.route('/query', methods=['GET', 'POST'])
-# def query():
-#     if request.method == "POST":
-#         if url_query is None:
-#             return jsonify_str({'error': 'Please upload or add link image.'})
-#         results = query_img(url_query, data)
-#         print('Finished query----------------')
-#         print(results)
-#         return jsonify_str({"results": results})
 
 @app.route('/query', methods=['GET', 'POST'])
 def query():
@@ -48,7 +146,9 @@ def query():
         return jsonify_str({"results": results})
 
 @app.route("/addlogo2json", methods=["GET", "POST"])
-def add_logo2json(logo):
+def add_logo2json():
+    image, filename = parse_args()
+    logo = image[0]
     if request.method == "POST":
         if logo is None:
             return jsonify_str({"error", "Please upload logo as input"})
@@ -57,25 +157,24 @@ def add_logo2json(logo):
         result = Q.add_logo2json(img)
         return jsonify_str(result)
 
-
-
-
 @app.route("/checklogo", methods=["GET", "POST"])
-def check_logo(img):
+def check_logo():
+    img, filenames = parse_args()
     if request.method == "POST":
         if img is None:
             return jsonify_str({"error": "Please upload photos as input"})
         Q = Query_Image()
-        img = Q.read_img(img)
-        result = Q.check_img_have_logo(img)
+        # img = Q.read_img(img)
+        result = Q.check_img_have_logo(img[0])
         if result:
-            return jsonify_str({"result": "Image have logo"})
+            return jsonify_str({"result": "Image have pepsi logo"})
         else:
-            return jsonify_str({"result": "Image not have logo"})
+            return jsonify_str({"result": "Image not have pepsi logo"})
         
 
 @app.route("/compare", methods=["GET", "POST"])
-def compare(img1, img2):
+def compare():
+    img1, img2 = None, None
     if request.method == "POST":
         if img1 is None or img2 is None:
             return jsonify_str({"error": "Please chooses or upload two photos as input"})
